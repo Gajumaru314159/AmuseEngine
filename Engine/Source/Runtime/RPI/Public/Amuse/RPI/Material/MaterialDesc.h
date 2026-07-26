f2319b21b10f0c01157f8594bb2598ed4a34be2e
Gajumaru <igm6323@gmail.com>
2026-07-26 22:28:57 +0900

    インデント調整

--- Engine/Source/Runtime/RPI/Public/Amuse/RPI/Material/MaterialDesc.h
+++ Engine/Source/Runtime/RPI/Public/Amuse/RPI/Material/MaterialDesc.h
@@ -16,10 +16,10 @@ namespace Amuse {
     //! @ingroup AmuseRPI
     struct InputLayout {
 
-        Semantic	    semantic;	//!< セマンティクス
+        Semantic	semantic;	//!< セマンティクス
         ElementType	type;		//!< コンポーネント型
-        s32			        dimention;	//!< 次元数
-        s32			        index;		//!< セマンティクス内インデックス
+        s32			dimention;	//!< 次元数
+        s32			index;		//!< セマンティクス内インデックス
 
     public:
 
@@ -35,18 +35,18 @@ namespace Amuse {
     //! @brief マテリアルパスの品質別シェーダ設定
     //! @ingroup AmuseRPI
     struct ShaderSet {
-		s32                             quality; //!< 品質値
+		s32                         quality;        //!< 品質値
 
         // PipelineState周りの必須情報
-        Ref<Shader> 		        vs; //!< 頂点シェーダ
-        Ref<Shader> 		        ps; //!< ピクセルシェーダ
-        Vector<InputLayout>		        inputLayout; //!< 入力レイアウト
-        RenderTargetFormatArray    colors; //!< カラー出力フォーマット
-        Optional<TextureFormat>	depth; //!< 深度フォーマット
-		SampleDesc		            sample; //!< サンプル設定
-		BlendDescList		        blend; //!< ブレンド設定
-        RasterizerDesc		        rasterizer; //!< ラスタライザ設定
-        DepthStencilDesc	        depthStencil; //!< 深度ステンシル設定
+        Ref<Shader> 		        vs;             //!< 頂点シェーダ
+        Ref<Shader> 		        ps;             //!< ピクセルシェーダ
+        Vector<InputLayout>		    inputLayout;    //!< 入力レイアウト
+        RenderTargetFormatArray     colors;         //!< カラー出力フォーマット
+        Optional<TextureFormat>	    depth;          //!< 深度フォーマット
+		SampleDesc		            sample;         //!< サンプル設定
+		BlendDescList		        blend;          //!< ブレンド設定
+        RasterizerDesc		        rasterizer;     //!< ラスタライザ設定
+        DepthStencilDesc	        depthStencil;   //!< 深度ステンシル設定
     };
 
     //! @brief マテリアルの描画パス設定
@@ -60,16 +60,16 @@ namespace Amuse {
     //! @brief  マテリアル定義
     //! @ingroup AmuseRPI
     struct MaterialShaderDesc{
-        String          name; //!< マテリアル名
+        String          name;       //!< マテリアル名
         
-        Vector<String>  textures; //!< テクスチャ名
-        Vector<String>  buffers; //!< バッファ名
-        Vector<String>  matrices; //!< 行列プロパティ名
-        Vector<String>  vectors; //!< ベクトルプロパティ名
-        Vector<String>  scalars; //!< スカラー名
-        Vector<String>  integers; //!< 整数名
+        Vector<String>  textures;   //!< テクスチャ名
+        Vector<String>  buffers;    //!< バッファ名
+        Vector<String>  matrices;   //!< 行列プロパティ名
+        Vector<String>  vectors;    //!< ベクトルプロパティ名
+        Vector<String>  scalars;    //!< スカラー名
+        Vector<String>  integers;   //!< 整数名
 
-        MaterialPassMap	passes; //!< 描画パス設定
+        MaterialPassMap	passes;     //!< 描画パス設定
     };
 
 }
